4da3168856c67724a2c51e92ecbba3fdd19b0702
Gabriel Ron <gaboarp2020@gmail.com>
2021-06-10 15:35:49 +0200

    Update setup.sh bas script

--- setup.sh
+++ setup.sh
@@ -4,17 +4,6 @@
 sudo pkill apache2
 sudo pkill postgres
 
-# Copy env variables
-# APP
-cp app/.env.example app/.env
-echo 'DATABASE_URL="postgresql://main:main@pgsql_db:5432/main?serverVersion=13&charset=utf8"' > app/.env
-
 # Build Docker Image
-docker-compose stop
+docker-compose down
 docker-compose up --build
-
-# Setup App
-docker-compose exec composer install --working-dir=/app
-docker-compose exec php_app php app/bin/console secrets:generate-keys
-docker-compose exec php_app php app/bin/console doctrine:schema:create
-docker-compose exec php_app php app/bin/console doctrine:migrations:migrate
